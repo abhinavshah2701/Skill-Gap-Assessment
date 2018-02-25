66f210d19e1f8e6f81ac2d5fc1981f472b9da3b4
Abhinav Shah <abhinavk.shah@gmail.com>
2018-02-25 17:00:12 +0530

    Header,Login And Footer Component Created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { LoginComponent } from './component/login/login.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
