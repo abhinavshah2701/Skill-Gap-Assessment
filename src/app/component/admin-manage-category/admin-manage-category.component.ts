4c823de7da8d63ed1ffba266d535a6d739202db0
Abhinav Shah <abhinavk.shah@gmail.com>
2018-03-29 15:46:37 +0530

    View Category Done With Database.

--- src/app/component/admin-manage-category/admin-manage-category.component.ts
+++ src/app/component/admin-manage-category/admin-manage-category.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../service/http.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-admin-manage-category',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminManageCategoryComponent implements OnInit {
 
-  constructor() { }
+  categories;
+
+  constructor(private httpService: HttpService,private router: Router) {
+    this.getData();
+   }
 
   ngOnInit() {
+  }
+
+  getData()
+  {
+    this.httpService.get('request_handler.php',{viewCategory:'viewCategory'}).then((response)=>{
+      if (response['success'] == true) {
+        this.categories = response['data'];
+      }
+      // console.log(this.categories);      
+    });
   }
 
 }
