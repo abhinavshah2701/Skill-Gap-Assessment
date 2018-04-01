b3a27409b42a6bf45d982f1cce88c2739c7bb073
Abhinav Shah <abhinavk.shah@gmail.com>
2018-04-02 02:13:49 +0530

    Delete Category Done With Database.

--- src/app/component/admin-manage-category/admin-manage-category.component.ts
+++ src/app/component/admin-manage-category/admin-manage-category.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../../service/http.service';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr'; 
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
@@ -15,12 +15,12 @@ export class AdminManageCategoryComponent implements OnInit {
   categoryForm: FormGroup;
   categories;
 
-  constructor(private httpService: HttpService,private formBuilder: FormBuilder,private router: Router,private toastr: ToastrService) {
+  constructor(private httpService: HttpService, private formBuilder: FormBuilder, private router: Router, private toastr: ToastrService) {
     this.createForm();
     this.getCategoryData();
-   }
+  }
 
-   ngOnInit() {
+  ngOnInit() {
     this.createForm();
   }
 
@@ -29,30 +29,41 @@ export class AdminManageCategoryComponent implements OnInit {
       categoryname: ['', Validators.required]
     });
   }
-  
 
-  onSubmit(values)
-  {
+
+  onSubmit(values) {
     values['addCategory'] = true;
-    this.httpService.post('request_handler.php',values).then((response)=>{
+    this.httpService.post('request_handler.php', values).then((response) => {
       console.log(response);
-      if(response['success']==true){
-        this.toastr.success('Category Added','',{timeOut:3000,closeButton:true,progressBar:true});
+      if (response['success'] == true) {
+        this.toastr.success('Category Added', '', { timeOut: 3000, closeButton: true, progressBar: true });
         this.getCategoryData();
         this.createForm();
       }
-      else{
-        this.toastr.error(response['error_message'],'',{timeOut:3000,closeButton:true,progressBar:true});
+      else {
+        this.toastr.error(response['error_message'], '', { timeOut: 3000, closeButton: true, progressBar: true });
       }
     });
     console.log(values);
   }
 
-  getCategoryData()
-  {
-    this.httpService.get('request_handler.php',{viewCategory:'viewCategory'}).then((response)=>{
+  getCategoryData() {
+    this.httpService.get('request_handler.php', { viewCategory: 'viewCategory' }).then((response) => {
       if (response['success'] == true) {
         this.categories = response['data'];
+      }
+    });
+  }
+
+  deleteCategory(id) {
+    this.httpService.post('request_handler.php', { deleteCategory: true, category_id: id }).then((response) => {
+      console.log(response);
+      if (response['success'] == true) {
+        this.toastr.success('Category Deleted', '', { timeOut: 3000, closeButton: true, progressBar: true });
+        this.getCategoryData();
+      }
+      else {
+        this.toastr.error(response['error_message'], '', { timeOut: 3000, closeButton: true, progressBar: true });
       }
     });
   }
