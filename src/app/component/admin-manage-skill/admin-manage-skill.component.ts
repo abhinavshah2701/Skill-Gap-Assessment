f0a0b8ece62588d40345a97e635754832e5fe6b6
Abhinav Shah <abhinavk.shah@gmail.com>
2018-04-02 14:22:19 +0530

    Dependent Selection Done On Category And Sub Category While Adding Skill.

--- src/app/component/admin-manage-skill/admin-manage-skill.component.ts
+++ src/app/component/admin-manage-skill/admin-manage-skill.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../../service/http.service';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr'; 
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-admin-manage-skill',
@@ -16,14 +16,13 @@ export class AdminManageSkillComponent implements OnInit {
   subcategories;
   skills;
 
-  constructor(private httpService: HttpService,private formBuilder: FormBuilder,private router: Router,private toastr: ToastrService) {
+  constructor(private httpService: HttpService, private formBuilder: FormBuilder, private router: Router, private toastr: ToastrService) {
     this.createForm();
     this.getCategoryData();
-    this.getSubCategoryData();
     this.getSkillData();
-   }
+  }
 
-   ngOnInit() {
+  ngOnInit() {
     this.createForm();
   }
 
@@ -34,50 +33,55 @@ export class AdminManageSkillComponent implements OnInit {
       skillname: ['', Validators.required]
     });
   }
-  
 
-  onSubmit(values)
-  {
+
+  onSubmit(values) {
     values['addSkill'] = true;
-    this.httpService.post('request_handler.php',values).then((response)=>{
+    this.httpService.post('request_handler.php', values).then((response) => {
       console.log(response);
-      if(response['success']==true){
-        this.toastr.success('Skill Added','',{timeOut:3000,closeButton:true,progressBar:true});
+      if (response['success'] == true) {
+        this.toastr.success('Skill Added', '', { timeOut: 3000, closeButton: true, progressBar: true });
         this.getCategoryData();
-        this.getSubCategoryData();
         this.getSkillData();
         this.createForm();
       }
-      else{
-        this.toastr.error(response['error_message'],'',{timeOut:3000,closeButton:true,progressBar:true});
+      else {
+        this.toastr.error(response['error_message'], '', { timeOut: 3000, closeButton: true, progressBar: true });
       }
     });
     console.log(values);
   }
 
-  getCategoryData()
-  {
-    this.httpService.get('request_handler.php',{viewCategory:'viewCategory'}).then((response)=>{
+  getCategoryData() {
+    this.httpService.get('request_handler.php', { viewCategory: 'viewCategory' }).then((response) => {
       if (response['success'] == true) {
         this.categories = response['data'];
+        this.getSubCategoryData();
       }
     });
   }
 
-  getSubCategoryData()
-  {
-    this.httpService.get('request_handler.php',{viewSubCategory:'viewSubCategory'}).then((response)=>{
+  getSubCategoryData() {
+    this.httpService.get('request_handler.php', { viewSubCategory: 'viewSubCategory' }).then((response) => {
       if (response['success'] == true) {
         this.subcategories = response['data'];
       }
     });
   }
 
-  getSkillData()
-  {
-    this.httpService.get('request_handler.php',{viewSkill:'viewSkill'}).then((response)=>{
+  getSkillData() {
+    this.httpService.get('request_handler.php', { viewSkill: 'viewSkill' }).then((response) => {
       if (response['success'] == true) {
         this.skills = response['data'];
+      }
+    });
+  }
+
+  onSelect(category_id) {
+    this.httpService.get('request_handler.php', { viewSubCategory: 'viewSubCategory' }).then((response) => {
+      if (response['success'] == true) {
+        this.subcategories = response['data'];
+        this.subcategories = this.subcategories.filter((data) => data.category_id == category_id);
       }
     });
   }
