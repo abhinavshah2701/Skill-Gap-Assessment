bb832f225bc3a7ed7cdb86e7b9f8d30db916ab2a
Abhinav Shah <abhinavk.shah@gmail.com>
2018-04-06 03:49:37 +0530

    Add Question Page Modified. Code For Add Question Created.

--- src/app/component/admin-add-question/admin-add-question.component.ts
+++ src/app/component/admin-add-question/admin-add-question.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnChanges, Input } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpService } from '../../service/http.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 import { QuestionClass, Option } from '../../data-model';
 
@@ -13,23 +16,25 @@ export class AdminAddQuestionComponent implements OnChanges {
 
   butDisabled: boolean = true;
   questionForm: FormGroup;
+  form_description: string;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private httpService: HttpService, private formBuilder: FormBuilder, private router: Router, private route: ActivatedRoute, private toastr: ToastrService) {
     this.createForm();
+    this.getFormData();
   }
 
 
   ngOnChanges() {
     // this.questionForm.reset({});
-    this.setOptions(this.question.options);
+    // this.setOptions(this.question.options);
   }
 
   createForm() {
-    this.questionForm = this.fb.group({
+    this.questionForm = this.formBuilder.group({
       questionname: '',
       questiontype: '',
       totalrating: '',
-      option_array: this.fb.array([])
+      option_array: this.formBuilder.array([])
     });
   }
 
@@ -38,21 +43,46 @@ export class AdminAddQuestionComponent implements OnChanges {
   };
 
   setOptions(options: Option[]) {
-    const optionFGs = options.map(option => this.fb.group(option));
-    const optionFormArray = this.fb.array(optionFGs);
+    const optionFGs = options.map(option => this.formBuilder.group(option));
+    const optionFormArray = this.formBuilder.array(optionFGs);
     this.questionForm.setControl('option_array', optionFormArray);
   }
 
   addOption() {
-    this.option_array.push(this.fb.group(new Option()));
+    this.option_array.push(this.formBuilder.group(new Option()));
   }
 
   removeOption(index) {
     this.option_array.removeAt(index);
   }
 
-  onSubmit() {
-    this.ngOnChanges();
+  onSubmit(values) {
+    // this.ngOnChanges();
+    values['addQuestion'] = true;
+    const id = this.route.snapshot.paramMap.get('id');
+    values['form_id'] = id;
+    this.httpService.post('request_handler.php', values).then((response) => {
+      if (response['success'] == true) {
+        this.toastr.success('Question Added', '', { timeOut: 3000, closeButton: true, progressBar: true });
+        this.router.navigate(['adminManageForm']);
+      }
+      else {
+        this.toastr.error(response['error_message'], '', { timeOut: 3000, closeButton: true, progressBar: true });
+      }
+    });
+    console.log(values);
+  }
+
+  getFormData() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.httpService.get('request_handler.php', { viewFormData: true, form_id: id }).then((response) => {
+      if (response['success'] == true) {
+        this.form_description = response['data'].form_name;
+      }
+      else {
+        this.toastr.error(response['error_message'], '', { timeOut: 3000, closeButton: true, progressBar: true });
+      }
+    });
   }
 
 }
