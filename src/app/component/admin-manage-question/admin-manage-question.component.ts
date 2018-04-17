96c8dc3b626f557c02e17c001387707151f0cb53
Abhinav Shah <abhinavk.shah@gmail.com>
2018-04-17 15:35:19 +0530

    Add Question Page Designed. Code for Add Question Created.

--- src/app/component/admin-manage-question/admin-manage-question.component.ts
+++ src/app/component/admin-manage-question/admin-manage-question.component.ts
@@ -1,19 +1,86 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, Input } from '@angular/core';
 import { HttpService } from '../../service/http.service';
-import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
+
+import { QuestionClass, Option } from '../../data-model';
 
 @Component({
   selector: 'app-admin-manage-question',
   templateUrl: './admin-manage-question.component.html',
   styleUrls: ['./admin-manage-question.component.scss']
 })
-export class AdminManageQuestionComponent implements OnInit {
+export class AdminManageQuestionComponent implements OnChanges {
+  @Input() question: QuestionClass;
 
-  constructor() { }
+  butDisabled: boolean = true;
+  questionForm: FormGroup;
+  form_description: string;
 
-  ngOnInit() {
+  constructor(private httpService: HttpService, private formBuilder: FormBuilder, private router: Router, private route: ActivatedRoute, private toastr: ToastrService) {
+    this.createForm();
+    this.getFormData();
   }
 
+  ngOnChanges() {
+    // this.questionForm.reset({});
+    // this.setOptions(this.question.options);
+  }
+
+  createForm() {
+    this.questionForm = this.formBuilder.group({
+      questionname: '',
+      questiontype: '',
+      totalrating: '',
+      option_array: this.formBuilder.array([])
+    });
+  }
+
+  get option_array(): FormArray {
+    return this.questionForm.get('option_array') as FormArray;
+  };
+
+  setOptions(options: Option[]) {
+    const optionFGs = options.map(option => this.formBuilder.group(option));
+    const optionFormArray = this.formBuilder.array(optionFGs);
+    this.questionForm.setControl('option_array', optionFormArray);
+  }
+
+  addOption() {
+    this.option_array.push(this.formBuilder.group(new Option()));
+  }
+
+  removeOption(index) {
+    this.option_array.removeAt(index);
+  }
+
+  onSubmit(values) {
+    // this.ngOnChanges();
+    values['addQuestion'] = true;
+    const id = this.route.snapshot.paramMap.get('id');
+    values['form_id'] = id;
+    this.httpService.post('request_handler.php', values).then((response) => {
+      if (response['success'] == true) {
+        this.toastr.success('Question Added', '', { timeOut: 3000, closeButton: true, progressBar: true });
+        // this.router.navigate(['adminManageForm']);
+      }
+      else {
+        this.toastr.error(response['error_message'], '', { timeOut: 3000, closeButton: true, progressBar: true });
+      }
+    });
+    console.log(values);
+  }
+
+  getFormData() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.httpService.get('request_handler.php', { viewFormData: true, form_id: id }).then((response) => {
+      if (response['success'] == true) {
+        this.form_description = response['data'].form_name;
+      }
+      else {
+        this.toastr.error(response['error_message'], '', { timeOut: 3000, closeButton: true, progressBar: true });
+      }
+    });
+  }
 }
--- src/app/data-model.ts
+++ src/app/data-model.ts
@@ -6,6 +6,8 @@ export class QuestionClass {
 }
 
 export class Option {
+  optionid = 0;
   optionname = '';
   optionrating = '';
+  correctoption = '';
 }
