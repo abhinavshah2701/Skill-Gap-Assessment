7b565e8f4c173f99380b3bd4ab9f7017d6275515
Abhinav Shah <abhinavk.shah@gmail.com>
2018-04-24 11:45:57 +0530

    Admin Update Category Component Removed.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,6 @@ import { AdminManageFormComponent }      from './component/admin-manage-form/adm
 import { AdminManageCategoryComponent }      from './component/admin-manage-category/admin-manage-category.component';
 import { AdminManageSubCategoryComponent } from './component/admin-manage-sub-category/admin-manage-sub-category.component';
 import { AdminManageSkillComponent } from './component/admin-manage-skill/admin-manage-skill.component';
-import { AdminUpdateCategoryComponent } from './component/admin-update-category/admin-update-category.component';
 import { AdminUpdateSubCategoryComponent } from './component/admin-update-sub-category/admin-update-sub-category.component';
 import { AdminUpdateSkillComponent } from './component/admin-update-skill/admin-update-skill.component';
 import { AdminUpdateFormComponent } from './component/admin-update-form/admin-update-form.component';
@@ -21,7 +20,6 @@ const routes: Routes = [
   { path: 'adminManageForm', component: AdminManageFormComponent },
   { path: 'adminUpdateForm/:id', component: AdminUpdateFormComponent },
   { path: 'adminManageCategory', component: AdminManageCategoryComponent },
-  { path: 'adminUpdateCategory/:id', component: AdminUpdateCategoryComponent },
   { path: 'adminManageSubCategory', component: AdminManageSubCategoryComponent },
   { path: 'adminUpdateSubCategory/:id', component: AdminUpdateSubCategoryComponent },
   { path: 'adminManageSkill', component: AdminManageSkillComponent },
